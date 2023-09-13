203ec2e062e6b86697f709265a4c87e6db61cf6d
yangxiaoping <2878847069@qq.com>
2023-09-13 14:52:48 +0800

    feat: 添加更新

---
+++ src/views/Scroll.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <table class="table" border="1" cellspacing="0" cellpadding="0">
+      <thead>
+        <tr>
+          <th class="forth">first</th>
+          <th class="forth">second</th>
+          <th class="forth">third</th>
+          <th class="forth">forth</th>
+          <th class="forth">forth</th>
+          <th class="forth">forth</th>
+          <th class="forth">forth</th>
+          <th class="forth">forth</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>123</td>
+          <td>123</td>
+          <td>123</td>
+          <td>123</td>
+          <td class="forth">123</td>
+          <td class="forth">123</td>
+          <td class="forth">123</td>
+          <td class="forth">123</td>
+        </tr>
+        <tr>
+          <td>123</td>
+          <td>123</td>
+          <td>123</td>
+          <td>123</td>
+          <td class="forth">123</td>
+          <td >123</td>
+          <td>123</td>
+          <td>123</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+.container {
+  width: 2000px;
+  border: 1px solid #000;
+  overflow: scroll;
+}
+.table {
+}
+.forth {
+  width: 100px;
+}
+th, td {
+  text-align: center;
+}
+</style>
